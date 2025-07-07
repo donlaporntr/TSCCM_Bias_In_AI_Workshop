efd5dc6afcd88e6bbe35ff5653c4e83d2907fd2c
Guntitat Sawadwuthikul <guntitat_sawadwuthikul@hotmail.com>
2025-07-07 20:08:13 +0700

    decomment

--- export.sh
+++ export.sh
@@ -2,5 +2,5 @@ fileNames=("AI_Fairness_in_Medicine" "AI_Fairness_in_Medicine_TH")
 
 for file in "${fileNames[@]}"; do
   jupyter nbconvert --clear-output --inplace --ClearMetadataPreprocessor.enabled=True $file.ipynb
-  # jupyter nbconvert --execute --to html $file.ipynb
+  jupyter nbconvert --execute --to html $file.ipynb
 done
