6ae8e5780944edb15ac084338473f59f9767ea21
my99N <my999n@gmail.com>
2025-07-07 20:08:53 +0700

    Enhance story (#7)
    * add eng suggestions
    
    * add thai suggestions
    
    * chmod export script
    
    * final touches
    
    * export
    
    * improve export.sh
    
    * clean up cell outputs
    
    * decomment
    
    ---------
    
    Co-authored-by: Guntitat Sawadwuthikul <guntitat_sawadwuthikul@hotmail.com>

--- export.sh
+++ export.sh
@@ -1,2 +1,6 @@
-jupyter nbconvert --execute --to html AI_Fairness_in_Medicine.ipynb
-jupyter nbconvert --execute --to html AI_Fairness_in_Medicine_TH.ipynb
+fileNames=("AI_Fairness_in_Medicine" "AI_Fairness_in_Medicine_TH")
+
+for file in "${fileNames[@]}"; do
+  jupyter nbconvert --clear-output --inplace --ClearMetadataPreprocessor.enabled=True $file.ipynb
+  jupyter nbconvert --execute --to html $file.ipynb
+done
